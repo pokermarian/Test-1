e813c8e68ba415335433aca5a6e91d5790727217
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-01-14 15:31:23 -0500

    upgrade dependencies; align with docs QS

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
-import 'dotenv/config';
-import express from 'express';
-import {
-  clerkClient,
-  clerkMiddleware,
-  getAuth,
-  requireAuth,
-} from '@clerk/express';
+import 'dotenv/config'
+import express from 'express'
+import { clerkClient, clerkMiddleware, getAuth, requireAuth } from '@clerk/express'
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3002
 
-app.use(clerkMiddleware());
+app.use(clerkMiddleware())
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!')
+})
 
 // Use requireAuth() to protect this route
+// If user is not authenticated, requireAuth() will redirect back to the homepage
 app.get('/protected', requireAuth(), async (req, res) => {
   // Use `getAuth()` to get the user's `userId`
-  const { userId } = getAuth(req);
+  // or you can use `req.auth`
+  const { userId } = getAuth(req)
 
   // Use Clerk's JavaScript Backend SDK to get the user's User object
-  const user = await clerkClient.users.getUser(userId);
+  const user = await clerkClient.users.getUser(userId)
 
-  res.json({ user });
-});
+  res.json({ user })
+})
 
+// Assuming you have a template engine installed and are using a Clerk JavaScript SDK on this page
+app.get('/sign-in', (req, res) => {
+  res.render('sign-in')
+})
+
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
